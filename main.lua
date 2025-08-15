1b46c888511b6b0ff142313169844b34dfa8d767
Arthur Faccio <arthur.faccio@proton.me>
2025-08-15 13:31:57 -0300

    First commit

---
+++ main.lua
@@ -0,0 +1,9 @@
+function love.draw()
+	local centerX = love.graphics.getWidth() / 2
+	local centerY = love.graphics.getHeight() / 2
+	love.graphics.circle("fill", centerX, centerY, 30)
+end
+
+function love.load() end
+
+function love.update() end
